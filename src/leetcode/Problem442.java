cfc380ef114a12ee9edb72e413776caa0e84ebc3
Tim Hin <hdhq_smile@buaa.edu.cn>
2017-11-22 17:42:12 +0800

    problem 442

--- src/leetcode/Problem442.java
+++ src/leetcode/Problem442.java
@@ -1,10 +1,14 @@
 package leetcode;
-
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Diane on 2017/11/18.
  * Find All Duplicates in an Array
  */
 public class Problem442 {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
 
+    }
 }
