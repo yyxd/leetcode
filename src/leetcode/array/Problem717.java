fa5ddf73727f5ec5283db83992c5df8472d179a2
hdhqsmile <hdhq_smile@buaa.edu.cn>
2018-03-29 09:19:00 +0800

    Problem 717 1-bit and 2-bit Characters Problem 724 Find Pivot Index Problem 495 Teemo Attacking

---
+++ src/leetcode/array/Problem717.java
@@ -0,0 +1,26 @@
+package leetcode.array;
+
+/**
+ * Created by Diane on 2017/12/29.
+ */
+public class Problem717 {
+    public static boolean isOneBitCharacter(int[] bits) {
+        int len = bits.length;
+        for(int i=0;i<len;i++)
+        {
+            if(bits[i]==1)
+            {
+                i++;
+                if(i==len-1) return false;
+            }
+            if(i==len-1&&bits[i]==1)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int []bits = {1,1,1,0,1,0,1,0,0,1,1,0,1};
+
+    }
+}
