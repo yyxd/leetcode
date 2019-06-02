c59879e261b8707b0d1d8986a4e1ff54c284d5b3
huangdan <hd1721@buaa.edu.cn>
2019-06-03 00:30:36 +0800

    contest 139 problem 2 数组翻转获取值全部相等的最大行数 problme 4 dp求元素和为目标值的子矩阵数量

--- src/contest139/Problem5075.java
+++ src/contest139/Problem5075.java
@@ -1,8 +1,85 @@
 package contest139;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Diane on 2019-06-02.
- * Goal:
+ * Goal: 元素为目标值的矩阵数量
  */
 public class Problem5075 {
+    public  int numSubmatrixSumTarget(int[][] matrix, int target) {
+        if(matrix.length<=0) return 0;
+        int[][] sum = new int[matrix.length][matrix[0].length];
+        sum[0][0] = matrix[0][0];
+        for (int i = 1; i < sum.length; i++) {
+            sum[i][0] = matrix[i][0]+sum[i-1][0];
+        }
+        for (int j = 1; j < sum[0].length; j++) {
+            sum[0][j] = matrix[0][j]+sum[0][j-1];
+        }
+        for(int i=1;i<sum.length;i++){
+            for(int j=1;j<sum[i].length;j++){
+                sum[i][j] = matrix[i][j]+sum[i-1][j]+sum[i][j-1]-sum[i-1][j-1];
+            }
+        }
+        int ans = 0;
+        for(int si=0;si<sum.length;si++){
+            for(int sj=0;sj<sum[si].length;sj++)
+            for (int ei=si;ei<sum.length;ei++){
+                for(int ej = sj;ej<sum[ei].length;ej++)
+                {
+                    if(target ==getAns(si,sj,ei,ej,sum))
+                       ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * hash map 空间换时间 O(n^3)
+     * @param a
+     * @param target
+     * @return
+     */
+    public int anotherNumSubmatrixSumTarget(int[][] a,int target){
+        int n = a.length, m = a[0].length;
+        int[][] cum = new int[n+1][m+1];
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < m;j++){
+                cum[i+1][j+1] = cum[i+1][j] + cum[i][j+1] - cum[i][j] + a[i][j];
+            }
+        }
+
+        int ans = 0;
+        for(int i = 0;i < n;i++){
+            for(int j = i;j < n;j++){
+                Map<Integer, Integer> map = new HashMap<>();
+                for(int k = 0;k <= m;k++){
+                    int v = cum[j+1][k] - cum[i][k];
+                    if(map.containsKey(v - target)){
+                        ans += map.get(v-target);
+                    }
+                    if(map.containsKey(v)){
+                        map.put(v, map.get(v)+1);
+                    }else{
+                        map.put(v, 1);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+    public  int getAns(int starti,int startj,int endi,int endj,int[][] sum){
+        if(starti-1<0 && startj-1<0) return sum[endi][endj];
+        if(starti-1<0) return sum[endi][endj]-sum[endi][startj-1];
+        if(startj-1<0)
+            return sum[endi][endj]-sum[starti-1][endj];
+        return sum[endi][endj]+sum[starti-1][startj-1]-sum[starti-1][endj]-sum[endi][startj-1];
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {{1,-1},{-1,1}};
+//        System.out.println(numSubmatrixSumTarget(matrix,0));
+    }
 }
--- src/contest139/Problem5077.java
+++ src/contest139/Problem5077.java
@@ -1,5 +1,7 @@
 package contest139;
 
+import java.util.HashMap;
+
 /**
  * Created by Diane on 2019-06-02.
  * Goal: 二维矩阵按照列翻转得到相同的最大行数
@@ -18,25 +20,49 @@ public class Problem5077 {
                     re = true;
                 }
                 for (int k = 0; k < m; k++) {
-                    if (re){
-                        if (matrix[i][k] == matrix[j][k]){
+                    if (re) {
+                        if (matrix[i][k] == matrix[j][k]) {
                             succ = false;
                             break;
                         }
                     } else {
-                        if (matrix[i][k] != matrix[j][k]){
+                        if (matrix[i][k] != matrix[j][k]) {
                             succ = false;
                             break;
                         }
                     }
                 }
-                if (succ){
-                    p ++;
+                if (succ) {
+                    p++;
                 }
             }
             ans = Math.max(ans, p);
         }
 
         return ans;
+    }
+
+    public static int anotherMaxEqualRowsAfterFlips(int[][] matrix) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < matrix.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            StringBuilder reverseSb = new StringBuilder();
+            for (int j = 0; j < matrix[i].length; j++) {
+                sb.append(matrix[i][j]);
+                reverseSb.append(1 - matrix[i][j]);
+            }
+            map.put(sb.toString(), map.getOrDefault(sb.toString(), 0) + 1);
+            map.put(reverseSb.toString(), map.getOrDefault(reverseSb.toString(), 0) + 1);
+        }
+        int ans = 1;
+        for (String str : map.keySet()) {
+            ans = Math.max(ans, map.get(str));
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {{0, 0, 0}, {0, 0, 1}, {1, 1, 0}};
+        System.out.println(anotherMaxEqualRowsAfterFlips(matrix));
+
     }
 }
